7a184439cf22bc19408bef0b1b069958fafb18f9
DaniilKostashchuk <125457617+DaniilKostashchuk@users.noreply.github.com>
2025-02-28 20:34:05 +0700

    Create U-Net.py

---
+++ U-Net/U-Net.py
@@ -0,0 +1,8 @@
+num_classes = 6  # Пример: 6 классов
+model = Unet(
+    encoder_name="resnet18",  # Используем ResNet18 в качестве энкодера
+    encoder_weights="imagenet",  # Предобученные веса на ImageNet
+    in_channels=1,  # 1 канал для grayscale изображений
+    classes=num_classes,  # Количество классов
+    activation="softmax",  # Софтмакс для многоклассовой классификации
+)
